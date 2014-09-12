87ae197aa05f16d8324abc041d65c0b46ca641b8
Husterknupp <4.23@gmx.de>
2014-09-12 08:03:25 +0200

    #2 cover more sort scenarios with UT

--- src/test/java/de/bschandera/tryouts/MergesortSimpleTest.java
+++ src/test/java/de/bschandera/tryouts/MergesortSimpleTest.java
@@ -1,20 +1,35 @@
 package de.bschandera.tryouts;
 
-import com.google.common.collect.Lists;
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
-import static org.fest.assertions.Assertions.assertThat;
+import com.google.common.collect.Lists;
 
 public class MergesortSimpleTest {
 
-    @Test
-    public void testSort() {
-        MergesortStrategy lucy = MergesortSimple.getInstance();
+	MergesortStrategy lucy = MergesortSimple.getInstance();
 
-        assertThat(lucy.sort(Lists.newArrayList(0))).isEqualTo(Lists.newArrayList(0));
-        assertThat(lucy.sort(Lists.newArrayList(2, 1))).isEqualTo(Lists.newArrayList(1, 2));
-        assertThat(lucy.sort(Lists.newArrayList(9, 8, 7, 6, 5, 4, 3, 2, 1, 0))).
-                isEqualTo(Lists.newArrayList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-    }
+	@Test
+	public void testSort() {
+		assertThat(lucy.sort(Lists.newArrayList(0))).isEqualTo(Lists.newArrayList(0));
+		assertThat(lucy.sort(Lists.newArrayList(2, 1))).isEqualTo(Lists.newArrayList(1, 2));
+		assertThat(lucy.sort(Lists.newArrayList(9, 8, 7, 6, 5, 4, 3, 2, 1, 0))).isEqualTo(
+				Lists.newArrayList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+	}
+
+	@Test
+	public void testSortWithBorders() {
+		assertThat(lucy.sort(Lists.newArrayList(0, Integer.MAX_VALUE, Integer.MIN_VALUE))).isEqualTo(
+				Lists.newArrayList(Integer.MIN_VALUE, 0, Integer.MAX_VALUE));
+	}
+
+	@Test
+	public void testSortWithEgalSequence() {
+		assertThat(lucy.sort(Lists.newArrayList(7, 5, 3, 5))).isEqualTo(Lists.newArrayList(3, 5, 5, 7));
+	}
 
 }
