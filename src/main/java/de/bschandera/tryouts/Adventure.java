0b18c17f5c52dced58fed17408f940f5bd121ecd
Husterknupp <4.23@gmx.de>
2014-09-27 16:07:20 +0200

    remove TODOs

--- src/main/java/de/bschandera/tryouts/Adventure.java
+++ src/main/java/de/bschandera/tryouts/Adventure.java
@@ -10,11 +10,6 @@ import org.apache.log4j.PropertyConfigurator;
 public class Adventure {
 	private static final Logger LOG = Logger.getLogger(Adventure.class);
 
-	// TODO document: this project is not about a fast implementation of Mergesort 
-	// but about the difference between multi- and singlethreaded versions of the same implementation
-
-	// TODO mach viele Läufe, um gesunden Mittelwert bilden zu können
-
 	public static void main(String[] args) {
 		initializeLogger("log4j.properties");
 
@@ -23,8 +18,6 @@ public class Adventure {
 		LOG.info("(c): " + Runtime.getRuntime().availableProcessors());
 
 		// Actual Sorting
-		// TODO document these counts online (the time for sorting them differs in a better way)
-		// TODO why takes it 0 ms to sort 100? (Happens after a few runs) 
 		sortMNumbersNTimes(MergesortStrategy.mergesortSimple(), 100, 300);
 		sortMNumbersNTimes(MergesortStrategy.mergesortSimple(), 10000, 300);
 		sortMNumbersNTimes(MergesortStrategy.mergesortSimple(), 100000, 300);
@@ -43,7 +36,6 @@ public class Adventure {
 	}
 
 	private static List<Integer> mRandomNumbers(int m) {
-		// TODO linear oder randomisierte Nummmern?
 		List<Integer> result = new ArrayList<>(m);
 		Random random = new Random();
 		for (int i = 0; i < m; i++) {
